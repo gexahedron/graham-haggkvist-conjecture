9a663e0cb0ddef0752ff88e3f741c928839f7598
Nikolay Ulyanov <ulyanick@gmail.com>
2018-04-12 17:21:08 +0300

    Lint

--- arbitrary_regular_set_of_trees/main.cpp
+++ arbitrary_regular_set_of_trees/main.cpp
@@ -1,289 +1,367 @@
+/*
+ * File:   regular_digraph_decomposition
+ * Author: Nikolay Ulyanov
+ *
+ * Created sometime between May and June 2014
+ */
+
+#include <random>
 #include <iostream>
-#include <cstdio>
 #include <vector>
 #include <ctime>
-#include <cstdlib>
-#include <cmath>
 #include <algorithm>
 #include <set>
 #include <sstream>
-#include <random>
 #include "freetree.h"
 
 using namespace std;
 
-// TODO: rename everything so it makes sense
-
 const int maxn = 30;
+const int NONE = -1;
 
-clock_t beginClock, endClock;
-vector<pair<int, int>> piiEdges;
-bool doTakePair[maxn*maxn];
-int degIn[maxn];
-int degOut[maxn];
+clock_t begin_clock, end_clock;
+vector<pair<int, int>> pii_edges;
+bool take_pair[maxn * maxn];
+int deg_in[maxn];
+int deg_out[maxn];
 
-int dege[maxn][maxn];
-bool usedEdge[maxn][maxn];
-//int kind[maxn][maxn];
+int edge_deg[maxn][maxn];
+bool used_edge[maxn][maxn];
+int whose_edge[maxn][maxn];
+
+bool used_copy[maxn][maxn];
+int copy_count[maxn];
 
 int cnt;
 
-vector<int> gr[2][maxn]; // 0 - From, 1 - To
+vector<int> gr[2][maxn]; // 0 - from, 1 - to
 
 int n;
-const int treeSize = 6;
-const int min_n = treeSize + 3; // min_n > treeSize + 1 // TODO use camelCase everywhere
+const int tree_size = 3; // number of edges
+const int min_n = tree_size + 2;//2 * tree_size + 1; // min_n >= 2 * tree_size + 1
 const int max_n = min_n;
 const int max_iter = 20;
 
-int neib[maxn][treeSize + 1];
-bool isn[maxn][treeSize + 1];
-int curTree, curVertex;
-// TODO: fix all this treeSize + 1 to treeSize; don't mess with indices
+int neib[maxn][tree_size + 1];
+bool neibed[maxn][maxn];
+//bool is_n[maxn][tree_size + 1];
+//int v_part[maxn][tree_size + 1];
+// TODO: fix all this tree_size + 1 to tree_size; don't mess with indices
 
+int tree_indices[maxn];
+//int tree_types[maxn];
 
-int treeIndices[maxn];
+int factor_deg[maxn][maxn];
 
-/*
-int n, k;
-vector<vector<int>> g;
-vector<int> mt;
-vector<char> used;
- 
-bool try_kuhn(int v) {
-    if (used[v]) return false;
-    used[v] = true;
-    for (size_t i=0; i<g[v].size(); ++i) {
-        int to = g[v][i];
-        if (mt[to] == -1 || try_kuhn(mt[to])) {
-            mt[to] = v;
-            return true;
+bool GenEdge(int iter, int v) {
+  if (iter == tree_size + 1) {
+    if (v == n - 1) {
+      /*bool regular_count = true;
+      for (int v1 = 0; v1 < n; ++v1) {
+        set<int> trees;
+        for (int v2 = 0; v2 < n; ++v2) {
+          if (used_edge[v1][v2]) {
+            trees.insert(whose_edge[v1][v2]);
+          }
+          if (used_edge[v2][v1]) {
+            trees.insert(whose_edge[v2][v1]);
+          }
         }
+        if (trees.size() != tree_size + 1) {
+          regular_count = false;
+          break;
+        }
+      }
+      if (!regular_count) {
+        return false;
+      }*/
+      ++cnt;
+      return true; // use false for counting, true for detecting 0
+    }
+    else {
+      if (GenEdge(1, v + 1)) {
+        return true;
+      }
     }
     return false;
-}
+  }
 
-int main() {
-    mt.assign(k, -1);
-    vector<char> used1(n);
-    for (int i=0; i<n; ++i) {
-        for (size_t j=0; j<g[i].size(); ++j) {
-            if (mt[g[i][j]] == -1) {
-                mt[g[i][j]] = i;
-                used1[i] = true;
-                break;
-            }
+  const int v_in_tree = NFreeTree::tree_database[tree_indices[v]][iter - 1 + 2];
+  const int cur_v = neib[v][v_in_tree];
+  if (factor_deg[cur_v][iter] >= 2) {
+    //cerr << "WUT" << endl;
+    return false;
+  }
+  //const int dir = v_part[v][v_in_tree];
+
+  for (int dir = 0; dir < 2; ++dir) {
+    for (int i = 0; i < tree_size; ++i) {
+      int u = gr[dir][cur_v][i];
+
+      /*if (is_n[u][iter]) {
+        continue;
+      }*/
+      if (factor_deg[u][iter] == 2) {
+        continue;
+      }
+
+      if (factor_deg[u][iter] > 2) {
+        cerr << "WTF" << endl;
+      }
+
+      int v1 = cur_v;
+      int v2 = u;
+      if (dir == 1) {
+        swap(v1, v2);
+      }
+      if (used_edge[v1][v2]) {
+        continue;
+      }
+
+      /*// TODO: add description what is_bad here
+      bool is_bad = false;
+      for (int j = 0; j < iter - 1; ++j) {
+        is_bad = (u == neib[v][j]);
+        if (is_bad) {
+          break;
         }
-    }
-    for (int i=0; i<n; ++i) {
-        if (used1[i]) continue;
-        used.assign(n, false);
-        try_kuhn(i);
-    }
- 
-    for (int i=0; i<k; ++i) {
-        if (mt[i] != -1) {
-            printf("%d %d\n", mt[i]+1, i+1);
+      }
+      if (is_bad) {
+        continue;
+      }*/
+
+      if (neibed[v][u]) {
+        continue;
+      }
+
+      ++factor_deg[v1][iter];
+      ++factor_deg[v2][iter];
+      //is_n[u][iter] = true;
+      neib[v][iter] = u;
+      neibed[v][u] = true;
+      used_edge[v1][v2] = true;
+
+      /*whose_edge[v1][v2] = v;
+      const bool prev_used_copy_v1 = used_copy[v1][v];
+      const bool prev_used_copy_v2 = used_copy[v2][v];
+      if (!used_copy[v1][v]) {
+          used_copy[v1][v] = true;
+          ++copy_count[v1];
+      }
+      if (!used_copy[v2][v]) {
+          used_copy[v2][v] = true;
+          ++copy_count[v2];
+      }*/
+
+      //if (copy_count[v1] <= tree_size + 1 && copy_count[v2] <= tree_size + 1) {
+        if (GenEdge(iter + 1, v)) {
+          return true;
         }
+      //}
+
+      /*if (!prev_used_copy_v1) {
+          used_copy[v1][v] = false;
+          --copy_count[v1];
+      }
+      if (!prev_used_copy_v2) {
+          used_copy[v2][v] = false;
+          --copy_count[v2];
+      }*/
+
+      neibed[v][u] = false;
+      used_edge[v1][v2] = false;
+      //is_n[u][iter] = false;
+      --factor_deg[v1][iter];
+      --factor_deg[v2][iter];
     }
-}
-*/
-
-bool genEdge(int iter, int v) {
-	if (iter == treeSize + 1) {
-		if (v == n - 1) {
-            ++cnt;
-			return true; // use false for counting, true for detecting 0
-		}
-		else {
-			if (genEdge(1, v + 1))
-				return true;
-		}
-		return false;
-	}
-
-	int vInTree = treeDatabase[curTree][iter - 1 + 2];
-	int curV = neib[v][vInTree];
-    //for (int dir = 0; dir <= 1; ++dir) {
-	int dir = 0;// TODO both directions could work
-
-    vector<tuple<int, int, int, int>> forSort;
-	for (int i = 0; i < treeSize; ++i) {
-		int u = gr[dir][curV][i];
-		if (isn[u][iter])
-			continue;
-
-		int v1 = curV;
-		int v2 = u;
-		if (dir == 1)
-			swap(v1, v2);
-		if (usedEdge[v1][v2])
-			continue;
-
-        // TODO: add description what isBad here
-		bool isBad = false;
-		for (int j = 0; j < iter - 1; ++j) {
-			isBad = (u == neib[v][j]);
-            if (isBad)
-				break;
-		}
-		if (isBad)
-			continue;
-
-        isn[u][iter] = true;
-		neib[v][iter] = u;
-		usedEdge[v1][v2] = true;
-		//kind[v1][v2] = iter;
-			
-		if (genEdge(iter + 1, v))
-			return true;
-
-		usedEdge[v1][v2] = false;
-		isn[u][iter] = false;
-	}
-	return false;
+  }
+  return false;
 }
 
 int main(int argc, char** argv) {
-    //random_device rd;
-    //mt19937 gen(rd());
-    mt19937 gen(42);
+  random_device rd;
+  mt19937 g(rd());//42);//rd());
 
-	beginClock = clock();
-	bool wasWTF = false;
-	int nomIter = 0;
+  begin_clock = clock();
+  bool found_combination_without_solution = false;
+  int iter_num = 0;
 
-    freeTree(treeSize + 1);
-    cerr << "totalAmount=" << totalAmount << endl;
+  NFreeTree::FreeTree(tree_size + 1);
+  cerr << "total_amount=" << NFreeTree::total_amount << endl;
+  uniform_int_distribution<>dis(0, NFreeTree::total_amount - 1);
+  uniform_int_distribution<>dis2(0, 1);
 
-	while (!wasWTF) {
-		cerr << "iter=" << nomIter << "; ";
-		for (n = min_n; n <= max_n; ++n) {
-			cerr << "n=" << n << ": ";
-			if (wasWTF)
-				break;
+  while (!found_combination_without_solution) {
+    cerr << "iter=" << iter_num << "; ";
+    for (n = min_n; n <= max_n; ++n) {
+      cerr << "n=" << n << ": ";
+      if (found_combination_without_solution) {
+        break;
+      }
 
-            // random regular graph generator
-			piiEdges.clear();
+      pii_edges.clear();
 
-			for (int i = 0; i < n; ++i) {
-				for (int j = 0; j < n; ++j) {
-					if (i != j) {
-						piiEdges.push_back(make_pair(i,j));
-                    }
-                }
+      for (int i = 0; i < n; ++i) {
+        for (int j = 0; j < n; ++j) {
+          if (i != j) {
+            pii_edges.push_back(make_pair(i, j));
+          }
+        }
+      }
+
+      bool is_regular = false;
+      while (!is_regular) {
+        shuffle(pii_edges.begin(), pii_edges.end(), g);
+        for (int i = 0; i < n; ++i) {
+          deg_in[i] = 0;
+          deg_out[i] = 0;
+        }
+
+        for (int i = 0; i < n; ++i) {
+          for (int j = 0; j < n; ++j) {
+            edge_deg[i][j] = 0;
+          }
+        }
+
+        for (int i = 0; i < (int) pii_edges.size(); ++i) {
+          take_pair[i] = false;
+          int v1, v2;
+          v1 = pii_edges[i].first;
+          v2 = pii_edges[i].second;
+          if (/*edge_deg[v2][v1] == 0 &&*/ (deg_out[v1] < tree_size) && (deg_in[v2] < tree_size)) {
+            take_pair[i] = true;
+            edge_deg[v1][v2] = 1;
+            ++deg_out[v1];
+            ++deg_in[v2];
+          }
+        }
+        is_regular = true;
+        for (int i = 0; i < n; ++i) {
+          is_regular = is_regular && (deg_in[i] == tree_size) && (deg_out[i] == tree_size);
+          if (!is_regular) {
+            break;
+          }
+        }
+      }
+
+      for (int i = 0; i < n; ++i) {
+        gr[0][i].clear();
+        gr[1][i].clear();
+      }
+
+      /*for (int i = 0; i < n; ++i) {
+        for (int j = 0; j < n; ++j) {
+          edge_deg[i][j] = 0;
+        }
+      }
+
+      for (int i = 0; i < (int) pii_edges.size(); ++i) {
+        int v1, v2;
+        v1 = pii_edges[i].first;
+        v2 = pii_edges[i].second;
+        if (take_pair[i]) {
+          edge_deg[v1][v2] = 1;
+        }
+      }*/
+
+      for (int i = 0; i < n; ++i) {
+        for (int j = 0; j < n; ++j) {
+          if (edge_deg[i][j] != 0) {
+            gr[0][i].push_back(j);
+            gr[1][j].push_back(i);
+          }
+        }
+      }
+
+      cerr << "trees: ";
+      for (int tree_idx = 0; tree_idx < n; ++tree_idx) {
+        tree_indices[tree_idx] = dis(g);
+        //const int cur_type = dis2(g);
+        //tree_types[tree_idx] = cur_type;
+        cerr << tree_indices[tree_idx] << " ";//"(" << cur_type << ") ";
+
+        //v_part[tree_idx][0] = cur_type;
+        for (int i = 0; i < tree_size; ++i) {
+          //v_part[tree_idx][i + 1] = 1 - v_part[tree_idx][NFreeTree::tree_database[tree_indices[tree_idx]][i + 2]];
+        }
+      }
+      cerr << endl;
+
+      for (int i = 0; i < n; ++i) {
+        for (int j = 0; j <= tree_size; ++j) {
+          //is_n[i][j] = false;
+          factor_deg[i][j] = 0;
+        }
+      }
+
+      for (int i = 0; i < n; ++i) {
+        copy_count[i] = 0;
+        for (int j = 0; j < n; ++j) {
+          used_edge[i][j] = false;
+          whose_edge[i][j] = NONE;
+          used_copy[i][j] = false;
+          neibed[i][j] = false;
+        }
+      }
+
+      for (int i = 0; i < n; ++i) {
+        neib[i][0] = i;
+      }
+
+      cnt = 0;
+      bool result = GenEdge(1, 0);
+
+      if (!cnt) {
+        cerr << "Found combination without solution!" << endl;
+        found_combination_without_solution = true;
+        cerr << "iter#=" << iter_num << endl;
+        cerr << "trees: ";
+        for (int i = 0; i < n; ++i) {
+          cerr << tree_indices[i] << " ";//"(" << tree_types[i] << ") ";
+        }
+        cerr << endl;
+        cerr << "trees:" << endl;
+        for (int j = 0; j < n; ++j) {
+          cerr << "#" << j << ": ";
+          for (int i = 0; i <= tree_size; ++i) {
+            cerr << NFreeTree::tree_database[tree_indices[j]][i + 1] << " ";
+          }
+          cerr << endl;
+        }
+
+        /*cerr << "parts:" << endl;
+        for (int j = 0; j < n; ++j) {
+          cerr << "#" << j << ": ";
+          for (int i = 0; i <= tree_size; ++i) {
+            cerr << v_part[j][i] << " ";
+          }
+          cerr << endl;
+        }*/
+
+        cerr << "graph:" << endl;
+        for (int i = 0; i < n; ++i) {
+          cerr << i << ": ";
+          for (int j = 0; j < n; ++j) {
+            if (edge_deg[i][j] > 0) {
+              cerr << j << " ";
             }
+          }
+          cerr << endl;
+        }
+        cerr << endl;
+        cerr << endl;
+      }
+    }
+    ++iter_num;
+    end_clock = clock();
+    double elapsed_secs = double(end_clock - begin_clock) / CLOCKS_PER_SEC;
+    begin_clock = end_clock;
+    cerr << "time: " << elapsed_secs << "s" << endl;
+  }
 
-            bool isRegular = false;
-            while (!isRegular) {
-                shuffle(piiEdges.begin(), piiEdges.end(), gen);
-                for (int i = 0; i < n; ++i) {
-                    degIn[i] = 0;
-                    degOut[i] = 0;
-                }
-                for (int i = 0; i < (int) piiEdges.size(); ++i) {
-                    doTakePair[i] = false;
-                    int v1, v2;
-                    v1 = piiEdges[i].first;
-                    v2 = piiEdges[i].second;
-                    if ((degOut[v1] < treeSize) && (degIn[v2] < treeSize)) {
-                        doTakePair[i] = true;
-                        ++degOut[v1];
-                        ++degIn[v2];
-                    }
-                }
-                bool allIsGood = true;
-                for (int i = 0; i < n; ++i)
-                    allIsGood = allIsGood && (degIn[i] == treeSize) && (degOut[i] == treeSize);
-                if (allIsGood)
-                    isRegular = true;
-            }
-
-            for (int i = 0; i < n; ++i) {
-                gr[0][i].clear();
-                gr[1][i].clear();
-            }
-            for (int i = 0; i < n; ++i)
-                for (int j = 0; j < n; ++j) {
-                    dege[i][j] = 0;
-                    //kind[i][j] = 0;
-                }
-
-            for (int i = 0; i < (int) piiEdges.size(); ++i) {
-                int v1, v2;
-                v1 = piiEdges[i].first;
-                v2 = piiEdges[i].second;
-                if (doTakePair[i])
-                    dege[v1][v2] = 1;
-            }
-
-            for (int i = 0; i < n; ++i) {
-                for (int j = 0; j < n; ++j) {
-                    if (dege[i][j] != 0) {
-                        gr[0][i].push_back(j);
-                        gr[1][j].push_back(i);
-                    }
-                }
-            }
-
-            // TODO: create additional bipartite version of the graph
-
-            // random tree set generator (TODO maybe also a loop?)
-            uniform_int_distribution<> dis(0, totalAmount - 1);
-            for (int i = 0; i < n; ++i) {
-                treeIndices[i] = dis(gen);
-            }
-
-            // TODO: loop of 2-factorizations
-            // TODO: maximum random bipartite matching function
-            // random shuffle of vertices
-            // and then just try to put all trees
-
-            cerr << "tree#=";
-            for (curTree = 0; curTree < totalAmount - 1; ++curTree) {
-                cerr << curTree << ",";
-                for (int curType = 0; curType < 2; ++curType) {
-                    for (int i = 0; i < n; ++i)
-                        for (int j = 0; j <= treeSize; ++j)
-                            isn[i][j] = false;
-
-                    for (int i = 0; i < n; ++i)
-                        for (int j = 0; j < n; ++j)
-                                usedEdge[i][j] = false;
-
-                    for (int i = 0; i < n; ++i)
-                        neib[i][0] = i;
-
-                    cnt = 0;
-                    bool result = genEdge(1, 0);
-            
-                    if (!cnt) {
-                        cout << "WTF" << endl;
-                        wasWTF = true;
-                        cout << "iter#=" << nomIter << "; tree#=" << curTree << "; type#=" << curType << endl;
-                        cout << endl;
-                        for (int i = 0; i < n; ++i) {
-                            cout << i << ": "; 
-                            for (int j = 0; j < n; ++j)
-                                if (dege[i][j] > 0)
-                                    cout << j << " ";
-                            cout << endl;
-                        }
-                        cout << endl;
-                        cout << endl;
-                    }
-                }
-            }
-		}
-		++nomIter;
-		endClock = clock();
-		double elapsed_secs = double(endClock - beginClock) / CLOCKS_PER_SEC;
-		beginClock = endClock;
-		cerr << " time: " << elapsed_secs << "s" << endl;
-	}
-
-	endClock = clock();
-	double elapsed_secs = double(endClock - beginClock) / CLOCKS_PER_SEC;
-	cout << "Time: " << elapsed_secs << "s" << endl;
-	return 0;
+  end_clock = clock();
+  double elapsed_secs = double(end_clock - begin_clock) / CLOCKS_PER_SEC;
+  cerr << "time: " << elapsed_secs << "s" << endl;
+  return 0;
 }
+
